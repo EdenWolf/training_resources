1808947809ad9b15bccc8bdd41e2831f502104fb
EdenWolf <edenwolfson93@gmail.com>
2021-05-17 00:00:28 +0300

    added save function

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,17 +4,25 @@ import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
 
+const LOCAL_STORAGE_LIST_KEY = 'tasks.lists';
+
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []);
   const [showStatus, setStatus] = useState({ completed: false, incompleted: false, all: true })
+
+  function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(items));
+  }
 
   function addNewItem(itemTitle) {
     setItems([...items, { title: itemTitle, completed: false, id: (new Date()).getTime() }]);
+    save();
   }
 
   function removeItem(item) {
     const updateItems = items.filter(currentItem => item.title !== currentItem.title);
     setItems(updateItems);
+    save();
   }
 
   function toggleCompleteItem(item) {
@@ -24,11 +32,13 @@ function App() {
       return currentItem;
     });
     setItems(updateItems);
+    save();
   }
 
   function clearCompleted() {
     const updateItems = items.filter(currentItem => !currentItem.completed);
     setItems(updateItems);
+    save();
   }
 
   function showCompleted() {
